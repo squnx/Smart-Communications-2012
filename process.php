751ab667d865eaf0c4f0dadded4fcf4c14ae32ef
Ivan Song <mousewurx@gmail.com>
2024-07-23 22:30:45 -0700

    first commit

---
+++ process.php
@@ -0,0 +1,19 @@
+<?php
+ $to = "info@creativityatwork.org";
+ $subject = "Creativity At Work";
+ $email = $_POST["email"];
+ $body = "Contact Form Submission \n\n Email: $email";
+ $sent = mail($to, $subject, $body) ;
+ 
+if($sent)
+ {
+ print "Successfully Submitted. ";
+ }
+ else
+ {
+ print "There was a problem with sending the message";
+ }
+ 
+print "Thank You!";
+ 
+?>
